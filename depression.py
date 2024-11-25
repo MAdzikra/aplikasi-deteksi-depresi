6a341e74d7d752b2c3605abe11eab9db11e53c51
MAdzikra <muhammad22237@mail.unpad.ac.id>
2024-11-25 12:38:37 +0700

    update model

--- depression.py
+++ depression.py
@@ -29,7 +29,7 @@ X = df.drop(columns=["Number", "Depression State"])
 y = df["Depression State"]
 
 # Membagi dataset menjadi training dan testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 # Membuat model KNN
 knn = KNeighborsClassifier(n_neighbors=2, weights='distance')
